b30be820810ffe6d40880e85269b940cd91d117c
BABABUSOLA <queenbusola1@gmail.com>
2023-07-25 17:39:25 -0700

    101-cocktail_sort_list.c

--- sort.h
+++ sort.h
@@ -3,6 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,6 +27,7 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
